cee7118c358caaa607faee1b87b43df3ce99eab1
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-04-07 19:22:43 +0530

    Even or Odd
    basic Even or odd checking in two different way

---
+++ EVEN or ODD.py
@@ -0,0 +1,13 @@
+n = int(input("Enter a Number : "))
+
+if n % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+n = int(input("Enter a Number : "))
+
+if (n&1):
+    print("Odd")
+else:
+    print("Even")
