22bb5c37d0edb228b530b7f1f70007bae2503b19
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-04-08 00:20:33 +0530

    Even numbers in list
    finding a Even numbers in list using two types of method

---
+++ Even_num_in_List.py
@@ -0,0 +1,14 @@
+list_1 = [1,2,3,4,5,6,7,8,9,10]
+
+for i in list_1:
+    if i % 2 == 0:
+        print(i,end=" ")
+    else:
+        pass
+
+
+for i in list_1:
+  if (i&1):
+    pass
+  else:
+    print(i,end=" ")
