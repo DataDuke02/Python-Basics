dcd5d0fa5650ef497eee85419d28ff5785081b42
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-04-10 02:38:22 +0530

    Largest number
    Largest Number in list

---
+++ Largest_num_in_list.py
@@ -0,0 +1,10 @@
+list_1 = [12,54,6,45,78,1,564,3,4,41,24,6,741,58,1,57,123,74,52,56,42,65,4,2,85,4,2,8,42,7]
+largest = 0
+
+for i in list_1:
+    if i > largest:
+        largest = i
+    else:
+        pass
+
+print(largest)
