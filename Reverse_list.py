194311cc2d1646054195287e6a9652280d460038
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-04-11 01:53:48 +0530

    Add list reversal functionality in Reverse_list.py
    Implement list reversal using both list method and manual iteration.

---
+++ Reverse_list.py
@@ -0,0 +1,12 @@
+n = [545,56,54,564,564,654,65,45,454,5,45,4]
+
+n.reverse()
+print(n)
+
+numbers = [1, 2, 3, 4]
+reversed_list = []
+
+for i in range(len(numbers)-1, -1, -1):
+    reversed_list.append(numbers[i])
+
+print(reversed_list)
