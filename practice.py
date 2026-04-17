3174d9c1c8a956d320cb445b83ba520f1cb3fb11
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-04-17 23:21:15 +0530

    Practicing the basics
    relearning from the basics

---
+++ practice.py
@@ -0,0 +1,29 @@
+print("Hello! World")
+
+name = "Thiru"
+age = 25
+edu = "B.E"
+CGPA = 8.40
+
+print(f"Hello! {name}")
+
+a =2
+b = 4
+a,b = b,a
+
+print(a,b)
+
+p = int(input("Enter a princple : "))
+r = int(input("Enter a rate of interest : "))
+t = int(input("Enter a rate : "))
+
+si = (p*r*t)/100
+print(si)
+
+r =float(input())
+area = 3.14 * r * r
+print(area)
+
+c = int(input("Enter a celsius : "))
+f = (c*9/5)+32
+print(f)
