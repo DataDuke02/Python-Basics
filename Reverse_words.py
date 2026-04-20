d51a8c61adc6734af21aaeaca1d9a22f32e4052a
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-04-20 21:20:42 +0530

    reverse words
    reverse words from sentences

---
+++ Reverse_words.py
@@ -0,0 +1,5 @@
+# Reverse words in a sentence
+s3 = "hello how are yu doing this days mines are very dry but i keep going so nothing to worry for now even though there is something to worry".lower()
+s4 = s3[::-1]
+for ch in s4:
+    print(ch,end="")
