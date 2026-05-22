66a0f13b3e45755b11e7b0cb1e60d420f8eb1f7e
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-05-22 23:50:09 +0530

    Merge overlapping intervals
    Given a collection of intervals, merge all overlapping intervals

---
+++ merge.py
@@ -0,0 +1,11 @@
+# Given a collection of intervals, merge all overlapping intervals
+#  Merge overlapping intervals.
+def merge(intervals):
+    intervals.sort(key=lambda x: x[0])
+    result = []
+    for interval in intervals:
+        if not result or result[-1][1] < interval[0]:
+            result.append(interval)
+        else:
+            result[-1][1] = max(result[-1][1], interval[1])
+    return result
