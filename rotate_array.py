9520626e48416c62718bc26d0e239bc3a45a0655
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-05-26 23:54:17 +0530

    Create rotate_array.py

---
+++ rotate_array.py
@@ -0,0 +1,10 @@
+arr = list(map(int,input("Enter First Numbers : ").split()))
+
+last = arr[-1]
+
+for i in range(len(arr)-1,0,-1):
+    arr[i] = arr[i-1]
+
+arr[0] = last
+
+print("Rotated : ",arr)
