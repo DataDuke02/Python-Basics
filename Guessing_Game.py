4950a670a064b876c95776b8d35e6662487d8887
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-06-29 23:43:29 +0530

    Create Guessing_Game.py

---
+++ Guessing_Game.py
@@ -0,0 +1,14 @@
+import random
+
+secret = random.randint(1, 100)
+
+while True:
+    guess = int(input("Guess a number between 1 and 100: "))
+
+    if guess < secret:
+        print("Too low!")
+    elif guess > secret:
+        print("Too high!")
+    else:
+        print("Congratulations! You guessed correctly.")
+        break
