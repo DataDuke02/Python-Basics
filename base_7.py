22923e18ce3a5973608d2d3ecb293ebf3e9ea57a
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-08-01 16:39:50 +0530

    Create base_7.py

---
+++ base_7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convertToBase7(self, num: int) -> str:
+        if num == 0:
+            return "0"
+
+        sign = ""
+
+        if num < 0:
+            sign = "-"
+            num = -num
+
+        result = []
+
+        while num > 0:
+            result.append(str(num % 7))
+            num //= 7
+
+        return sign + "".join(result[::-1])
