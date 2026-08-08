7ea842979fb7a973ee60ad7999f2ed16189d7125
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-08-08 23:11:40 +0530

    Create countBalls.py

---
+++ countBalls.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countBalls(self, lowLimit: int, highLimit: int) -> int:
+        boxes = {}
+
+        for num in range(lowLimit, highLimit + 1):
+            temp = num
+            total = 0
+
+            while temp > 0:
+                total += temp % 10
+                temp //= 10
+
+            boxes[total] = boxes.get(total, 0) + 1
+
+        return max(boxes.values())
+
+
+
+Input:
+lowLimit = 1
+highLimit = 10
+
+Output:
+2
