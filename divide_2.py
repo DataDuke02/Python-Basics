552a4939235a46f455a2ddb65045f88270262f80
Thirugnansambantham S <thirugnanasambantham2802@gmail.com>
2026-08-21 00:04:01 +0530

    Create divide_2.py

---
+++ divide_2.py
@@ -0,0 +1,42 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+
+        negative = (dividend < 0) != (divisor < 0)
+
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        result = 0
+
+        while dividend >= divisor:
+            value = divisor
+            power = 1
+
+            while dividend >= (value << 1):
+                value <<= 1
+                power <<= 1
+
+            dividend -= value
+            result += power
+
+        if negative:
+            result = -result
+
+        return result
+
+Input:
+dividend = 10
+divisor = 3
+
+Output:
+3
+
+
+Input:
+dividend = 7
+divisor = -3
+
+Output:
+-2
